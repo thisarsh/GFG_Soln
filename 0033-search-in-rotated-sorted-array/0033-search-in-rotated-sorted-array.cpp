4a939e55c673c09c23d35ff42a28455e0c859354
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2025-11-21 15:41:51 +0530

    Time: 0 ms (100%), Space: 15.3 MB (32.14%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,21 +6,21 @@ public:
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]==target)return mid;
-            if(nums[mid]>=nums[low]){
-                if(nums[low]<=target && nums[mid]>target ){
-                    high=mid-1;                   
-
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && nums[mid]>target){
+                    high=mid-1;
                 }
                 else low=mid+1;
             }
             else{
-                 if(nums[mid]<target && nums[high]>=target ){
-                   low=mid+1;                 
-
+                if(nums[high]>=target && nums[mid]<target){
+                    low=mid+1;
                 }
-                else high=mid-1;
-
+                else{
+                    high=mid-1;
+                }
             }
+
         }
         return -1;
     }
