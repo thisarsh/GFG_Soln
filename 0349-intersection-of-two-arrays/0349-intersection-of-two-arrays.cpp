bc7457557437f4dd752a48d040bb3b39efcad1ae
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-03-28 17:27:28 +0530

    Time: 7 ms (10.69%), Space: 14.6 MB (14.93%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -5,7 +5,7 @@ public:
         unordered_set <int> st (nums1.begin(), nums1.end());
         // int p1=0,p2=0, n1=nums1.size(),n2=nums2.sise();
         for(auto i: nums2){
-            if(st.find(i)!=st.end() && ans.find(i)==ans.end() ) ans.insert(i);
+            if(st.find(i)!=st.end()  ) ans.insert(i);
         }
         vector <int> k(ans.begin(),ans.end());
         return k;
