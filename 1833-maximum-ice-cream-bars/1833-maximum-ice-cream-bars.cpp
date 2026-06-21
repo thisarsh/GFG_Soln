7dc024e72dfc533ff25df59d1efc71642c9860d8
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-21 17:01:00 +0530

    Time: 37 ms (37.14%), Space: 80.3 MB (37.51%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(),costs.end());
+        int c=0,ans=0;
+        for(auto it:costs){
+            if(c+it <= coins) {
+                ans++;
+                c+=it;
+            }
+            
+        }
+        return ans;
+    }
+};
