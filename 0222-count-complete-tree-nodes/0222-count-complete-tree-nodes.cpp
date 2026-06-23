dd7132dfc999ddf8884833f037b7f3cb400b63a5
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-23 22:45:55 +0530

    Time: 0 ms (100%), Space: 31.5 MB (16.9%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,8 +11,30 @@
  */
 class Solution {
 public:
+int lh(TreeNode *node){
+    if(!node)return 0;
+    int count=0;
+    while(node){
+        node=node->left;
+        count++;
+    }
+    return count;
+}
+int rh(TreeNode *node){
+    if(!node)return 0;
+    int count=0;
+    while(node){
+        node=node->right;
+        count++;
+    }
+    return count;
+}
+
     int countNodes(TreeNode* root) {
      if(!root) return 0;
+     if(lh(root)==rh(root)){
+        return pow(2,rh(root))-1;
+     }
      return 1+ countNodes(root->left)+countNodes(root->right);   
     }
 };
