5a599260bd6d0a5765660819b3b576c804c51308
Utkarsh Shukla <Shuklajimasthai@gmail.com>
2026-06-25 01:08:15 +0530

    Time: 0 ms (100%), Space: 10.8 MB (94.35%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -3,28 +3,42 @@
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
+ ----------opre----- root left right
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- //ROOT-LEFT-RIGHT
 class Solution {
 public:
-    void dfs(TreeNode *node, vector <int> &ans){
-        if(!node){
-            return;
-        }
-        ans.push_back(node->val);
-        dfs(node->left,ans);
-        dfs(node->right,ans);
-
-
-    }
     vector<int> preorderTraversal(TreeNode* root) {
+        TreeNode * curr=root;
         vector <int> ans;
-        dfs(root,ans);
+        while(curr){
+            if(!curr->left){
+                ans.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode * ip=curr->left;
+                while(ip->right && ip->right!=curr){
+                    ip=ip->right;
+                }
+                if(!ip->right){
+                    ans.push_back(curr->val);
+                    ip->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    ip->right=NULL;
+                    curr=curr->right;
+                }
+            }
+
+
+        }
         return ans;
+        
     }
 };
